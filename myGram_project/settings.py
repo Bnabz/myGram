503c0e63b25085bcdbb87f43dfefab1b42804889
Nabz <bnnabiswa@gmail.com>
2020-05-31 06:20:16 +0300

    create index view, url

--- myGram_project/settings.py
+++ myGram_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'myGram.apps.MyGramConfig',
+    'myGram.apps.MygramConfig',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
