6ec6f63ba316bd465651c51e422374e3fa66eb14
Nabz <bnnabiswa@gmail.com>
2020-05-31 16:06:44 +0300

    fail Profile model test

--- myGram/tests.py
+++ myGram/tests.py
@@ -4,7 +4,7 @@ from .models import Image,Profile,Comment
 
 class ImageTestCase(TestCase):
     def setUp(self):
-        self.user = User(username="test_name", password = "test_pass",email="testmail@gmail.com",)
+        self.user = User(username="test_name", password = "test_pass",email="testmail@gmail.com")
         self.user.save()
         self.profile = Profile(user=self.user, profile_pic='test.jpg',bio="test_bio",followers=0, following=0,)
         self.profile.save()
@@ -13,5 +13,33 @@ class ImageTestCase(TestCase):
 
         def test_instance(self):
             self.assertTrue(isinstance(self.image_test, Image))
-                                   
+
+        def tearDown(self):
+            User.objects.all().delete()
+            Profile.objects.all().delete()
+            Image.objects.all().delete()
+
+        
+
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User(username="test_name", password = "test_pass",email="testmail@gmail.com")
+        self.user.save()
+        self.profile = Profile(user=self.user, profile_pic='test.jpg',bio="test_bio",followers=0, following=0,)
+        self.profile.save()
+        
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile)) 
+
+    def test_search_profile(self):
+        self.user.save()
+        user = Profile.search_user(self.test_name)
+        self.assertEqual(len(user), 1)
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+        
                          
