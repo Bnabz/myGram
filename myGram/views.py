ebee9286c20a461a3c99e3ba9ad0aa411a888043
Nabz <bnnabiswa@gmail.com>
2020-06-01 10:30:15 +0300

    include register option in login

--- myGram/views.py
+++ myGram/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-from .models import Image, Comment, Profile,
+from .models import Image, Comment, Profile
+from django.contrib.auth.decorators import login_required
 
+# @login_required(login_url='/accounts/login/')
 def index(request):
     
     return render(request, 'index.html')
