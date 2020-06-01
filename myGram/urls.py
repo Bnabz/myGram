46903cceafe1deff1b501cb01d5547b28ff50b6c
Nabz <bnnabiswa@gmail.com>
2020-06-02 02:24:15 +0300

    add create_post html

--- myGram/urls.py
+++ myGram/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile/',views.profile, name='profile'),
+    path('create_post/', views.create_post, name='create_post'),
     
 ]
